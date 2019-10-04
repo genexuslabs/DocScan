ed173ecde7a1d42da35b43df682ab781de19ed71
builder <builder@genexus.com>
2019-10-04 16:51:39 -0300

    Add upload to GeneXus' Nexus

--- lib/src/main/java/com/kranti/doc/scanner/scan/ScanPresenter.kt
+++ lib/src/main/java/com/kranti/doc/scanner/scan/ScanPresenter.kt
@@ -157,7 +157,7 @@ class ScanPresenter constructor(private val context: Context, private val iView:
                     val mat = Mat(Size(pictureSize?.width?.toDouble() ?: 1920.toDouble(),
                             pictureSize?.height?.toDouble() ?: 1080.toDouble()), CvType.CV_8U)
                     mat.put(0, 0, p0)
-                    val pic = Imgcodecs.imdecode(mat, Imgcodecs.CV_LOAD_IMAGE_UNCHANGED)
+                    val pic = Imgcodecs.imdecode(mat, Imgcodecs.IMREAD_UNCHANGED)
                     Core.rotate(pic, pic, Core.ROTATE_90_CLOCKWISE)
                     mat.release()
                     com.kranti.doc.scanner.SourceManager.corners = com.kranti.doc.scanner.processor.processPicture(pic)
